ab9c131c4135d50c8e959a3861b854ff4a8f259d
pavanipavulurii <23eg110c48@anurag.edu.in>
2026-05-14 09:38:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor, plot_tree
